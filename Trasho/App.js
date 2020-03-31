3de78b300dea4a4433fff80ca3149ff1cbd48c6c
Florian Barbet <florian.barbet.etu@univ-lille.fr>
2020-03-31 21:36:14 +0200

    Push des dernieres nouveauté vous allez aimer

--- Trasho/App.js
+++ Trasho/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import {
 
   INFINITE_ANIMATION_ITERATIONS,
@@ -10,101 +10,151 @@ import {
   MapShapeType
 
 } from "react-native-webview-leaflet";
-import { StyleSheet, Text, View, Dimensions, Alert } from 'react-native';
+import { Button } from "native-base";
+import { StyleSheet, Text, View, Dimensions,Alert } from 'react-native';
 import * as Location from "expo-location";
 import * as Permissions from "expo-permissions";
 
 
-export default class App extends React.Component {
-  ownPosition;
+export default function App() {
+  const [mapCenterPosition, setMapCenterPosition] = useState({
+    lat:  50.6436348,
+    lng: 3.0900153
+  });
+  const [ownPosition, setOwnPosition] = useState(null);
+  const [webViewLeaflet, setWebViewLeaflet] = useState(null);
 
-  onMessageReceived(message) {
-    switch (message.event) {
-      default:
-        //console.log("App received", message)
-        ;
-    }
-  }
+  const onMessageReceived = (message)=> {switch (message.event) {default:;}}
 
+  useEffect(() => {
+      getLocationAsync();
+  });
 
+  const getLocationAsync = async () => {
+      let { status } = await Permissions.askAsync(Permissions.LOCATION);
+      if (status !== "granted") {
+        console.warn("Permission to access location was denied");
+      }
 
-  getLocationAsync = async () => {
-    let { status } = await Permissions.askAsync(Permissions.LOCATION);
-    if (status !== "granted") {
-      console.warn("Permission to access location was denied");
-    }
+      let location = await Location.getCurrentPositionAsync({});
+      if (!ownPosition) {
+        setOwnPosition ({
+          lat: location.coords.latitude,
+          lng: location.coords.longitude
+        });
+      }
+    };
 
-    let location = await Location.getCurrentPositionAsync({});
-    if (!this.ownPosition) {
-      this.ownPosition = {
-        lat: location.coords.latitude,
-        lng: location.coords.longitude
-      };
-      console.log("Position : ", this.ownPosition);
-    }
-  };
-
-
-  render() {
-    (() => {
-      this.getLocationAsync();
-    })();
 
     return (
-      //<View >
-      <WebViewLeaflet
-        ref={(component) => (this.webViewLeaflet = component)}
-        onLoad={this.onLoad}
-        onMessageReceived={this.onMessageReceived}
-        eventReceiver={this}
-        mapLayers={[
-          {
-            attribution:
-              '&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-            baseLayerIsChecked: true,
-            baseLayerName: "OpenStreetMap.Mapnik",
-            url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          },
-          {
-            baseLayerName: "Mapbox",
-            url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-            // url: `https://api.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=${mapboxToken}`,
-            attribution:
-              "&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-          }
-        ]}
-        mapCenterPosition={this.ownPosition}
-        ownPositionMarker={
-          this.ownPosition && {
-            position: this.ownPosition,
-            icon: "O",
-            size: [32, 32],
-            animation: {
-              duration: getDuration(),
-              delay: getDelay(),
-              iterationCount,
-              type: AnimationType.BOUNCE
-            }
-          }
-        }
-        zoom={7}// the component that will receive map events
-      />
+      <View style = {styles.container} >
+      	<View style = {styles.header} >
+      		<Text style={styles.headerText}>React Native Webview Leaflet Demo</Text>
+        </View>
+      <View style={{ flex: 1 }}>
+      {
+        <WebViewLeaflet
+            ref={(component) => (setWebViewLeaflet(component))}
 
-      //  <MapView style={styles.mapStyle} />
-      //      </View>
+            onMessageReceived={onMessageReceived}
+            eventReceiver={this}
+            mapLayers={[{
+               attribution:
+                 '&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+               baseLayerIsChecked: true,
+               baseLayerName: "OpenStreetMap.Mapnik",
+               url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+             }]}
+           mapMarkers={[
+		{
+			id:'2',
+		       position: {lat:  50.9436348,lng: 3.0900153 },
+	               icon: "https://i.ya-webdesign.com/images/google-maps-pin-png-4.png",
+	               size: [24, 32],		
+		}
+	   ]}
+           mapCenterPosition={mapCenterPosition}
+           ownPositionMarker={
+             ownPosition && {
+               position: ownPosition,
+               icon: "https://www.stickpng.com/assets/images/58889219bc2fc2ef3a1860aa.png",
+               size: [24, 32],
+               /*animation: {
+                 duration: "0.5",
+                 delay: 0,
+                 iterationCount: INFINITE_ANIMATION_ITERATIONS,
+                 type: AnimationType.BOUNCE
+               } //si tu veux le voir bondir */
+             }
+           }
+           mapShapes={[
+              {
+                shapeType: MapShapeType.CIRCLE,
+                color: "#123123",
+                id: "1",
+                center:  mapCenterPosition ,
+                radius: 2000
+              }]}
+           zoom={50}
+/>
+}
+</View>
+
+<View style={styles.mapControls}>
+       <Button
+         onPress={() => {
+           getLocationAsync();
+           setMapCenterPosition(ownPosition);
+           if (webViewLeaflet)
+                webViewLeaflet.setMapCenterPosition();
+            console.log(ownPosition);
+         }}
+         style={styles.mapButton}
+         success
+       >
+         <Text style={styles.mapButtonEmoji}>🎯</Text>
+       </Button>
+       </View>
+      </View>
     );
-  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff"
   },
-  mapStyle: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+  header: {
+    height: 60,
+    backgroundColor: "dodgerblue",
+    paddingHorizontal: 10,
+    paddingTop: 30,
+    width: "100%"
   },
+  headerText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "600"
+  },
+  mapControls: {
+    backgroundColor: "rgba(255,255,255,.5)",
+    borderRadius: 5,
+    bottom: 25,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    left: 0,
+    marginHorizontal: 10,
+    padding: 7,
+    position: "absolute",
+    right: 0
+  },
+  mapButton: {
+    alignItems: "center",
+    height: 42,
+    justifyContent: "center",
+    width: 42
+  },
+  mapButtonEmoji: {
+    fontSize: 28
+  }
 });
