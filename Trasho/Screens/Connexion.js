39b4e5a72875215d27fe18a619d4bb12581e738c
Lucas <lucas.laloux3011@gmail.com>
2020-04-02 14:45:04 +0200

    Page de connexion, manque à ajouter le bouton d'inscription

--- Trasho/Screens/Connexion.js
+++ Trasho/Screens/Connexion.js
@@ -1,26 +1,101 @@
 //This is an example code for NavigationDrawer//
 import React, { Component } from 'react';
 //import react in our code.
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
 // import all basic components
 
-export default class Connexion extends Component {
-  //Screen2 Component
-  render() {
-    return (
-      <View style={styles.MainContainer}>
-        <Text style={{ fontSize: 23 }}> Connexion </Text>
-      </View>
-    );
+class Connexion extends Component {
+    state = {
+      email: '',
+      password: ''
   }
-}
+  handleEmail = (text) => {
+      this.setState({ email: text })
+  }
+  handlePassword = (text) => {
+      this.setState({ password: text })
+  }
+  login = (email, pass) => {
+      alert('email: ' + email + ' password: ' + pass)
+  }
+  render() {
+      return (
+        <View style = {styles.container}>
+        <Image
+                source={require('../Images/logo.png')}
+                style={ styles.logo }
+            />
 
-const styles = StyleSheet.create({
-  MainContainer: {
-    flex: 1,
-    paddingTop: 20,
-    alignItems: 'center',
-    marginTop: 50,
-    justifyContent: 'center',
+        <Text style={ styles.text }> Adresse email </Text>
+
+            <TextInput style = {styles.input}
+              underlineColorAndroid = "transparent"
+              placeholder = "Email"
+              placeholderTextColor = "#000000"
+              autoCapitalize = "none"
+              onChangeText = {this.handleEmail}/>
+
+            <Text style={ styles.text }> Mot de passe </Text>
+
+            <TextInput style = {styles.input}
+              underlineColorAndroid = "transparent"
+              placeholder = "Password"
+              placeholderTextColor = "#000000"
+              placeholderStyle={{ color: 'red' }}
+              autoCapitalize = "none"
+              onChangeText = {this.handlePassword}/>
+            
+            <TouchableOpacity
+              style = {styles.submitButton}
+              onPress = {
+                  () => this.login(this.state.email, this.state.password)
+              }>
+              <Text style = {styles.submitButtonText}> Connexion </Text>
+            </TouchableOpacity>
+            
+        </View>
+      )
+  }
+  }
+  export default Connexion
+
+  const styles = StyleSheet.create({
+  container: {
+      paddingTop: 23
   },
-});
+  input: {
+      marginRight: 15,
+      marginLeft: 15,
+      height: 40,
+      borderColor: '#000000',
+      borderWidth: 1,
+      paddingLeft: 10,
+      marginTop: 20
+  },
+  submitButton: {
+      backgroundColor: '#000000',
+      padding: 10,
+      marginTop: 40,
+      marginLeft: 15,
+      marginRight: 15,
+      height: 40,
+  },
+  submitButtonText:{
+      color: 'white',
+      textAlign: 'center',
+  },
+  text: {
+    fontSize: 23,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 15
+  },
+  logo: {
+    width: 250,
+    height: 250,
+    marginLeft: 10,
+    marginTop: 3,
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  }
+})
