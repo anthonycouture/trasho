2c9163b4eee92e5e637b6c069af53925ec2d5ffd
Anthony Couture <antobox28@gmail.com>
2020-04-03 12:25:35 +0200

    recupère type poubelle

--- Trasho/Components/ModalInfoPoubelle.js
+++ Trasho/Components/ModalInfoPoubelle.js
@@ -4,12 +4,41 @@ import { Image, Text, StyleSheet, View } from 'react-native';
 export default class ModalInfoPoubelle extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            idPoubelle: this.props.idPoubelle,
+            typePoubelle: null
+        }
+    }
+
+    setTypePoubelle(type) {
+        this.setState({ typePoubelle: type });
+    }
+
+    componentDidMount() {
+        this.getTypePoubelleAsync();
+    }
+
+
+    async getTypePoubelleAsync() {
+        const url = 'http://10.0.2.2:3000/getTypePoubelle?idPoubelle=' + this.state.idPoubelle
+        const response = await fetch(url)
+        const json = await response.json()
+        this.setTypePoubelle(
+            <View>
+                <Text>Type de la poubelle : </Text>
+                {json.map(item => (
+                    <Text>{item.type}</Text>
+                ))}
+            </View>
+
+        );
+
     }
 
     render() {
         return (
             <View>
-                <Text style={styles.modalText}>Id Poubelle : {this.props.idPoubelle}</Text>
+                {this.state.typePoubelle}
                 <Image
                     source={require('./../Images/drawer.png')}
                 />
