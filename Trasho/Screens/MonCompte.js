60a197bf2fd5269262775930c3ba510570d96de1
Lucas <lucas.laloux3011@gmail.com>
2020-04-06 18:37:40 +0200

    Design. Changer les text en Button pour pouvoir cliquer dessus.

--- Trasho/Screens/MonCompte.js
+++ Trasho/Screens/MonCompte.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 //import react in our code.
 import { StyleSheet, View } from 'react-native';
-import { Container, Content, Input, Card, CardItem, Text, Body, Item, Button } from "native-base";
+import { Container, Content, Input, Card, CardItem, Text, Body, Item, Button, Icon } from "native-base";
 import * as Progress from 'react-native-progress';
 // import all basic components
 
@@ -41,19 +41,21 @@ export default class MonCompte extends Component {
                     </Card>
                     <Card>
                         <CardItem bordered style={{ justifyContent: 'center', color: 'black' }}>
+                        <Icon name={'settings'} />
                             <Text>Récupérer mes données</Text>
                         </CardItem>
                     </Card>
                     <Card>
                         <CardItem bordered style={{ justifyContent: 'center', color: 'black' }}>
+                            <Icon name={'person'} />
                             <Text>Supprimer mon compte</Text>
                         </CardItem>
                     </Card>
                     <Text style={{marginRight: 'auto', marginLeft: 'auto', marginTop: 30 }}> Niveau </Text>
                     <Item style={{ borderColor: 'transparent', justifyContent: 'center', marginTop: 15 }}>
-                        <Text>1</Text>
+                        <Text style={{marginRight: 5}}>1</Text>
                         <Progress.Bar progress={0.5} width={300} borderColor={'#74992e'} color={'#74992e'} />
-                        <Text>2</Text>
+                        <Text style={{marginLeft: 5}}>2</Text>
                     </Item>
                 </Content>
             </Container>
