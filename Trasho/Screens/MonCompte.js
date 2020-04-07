1946d24a8ad88bd512ce30e0512a11131171a3bd
Lucas <lucas.laloux3011@gmail.com>
2020-04-07 10:20:59 +0200

    Ajout d'actions sur les input + voir/cacher mdp

--- Trasho/Screens/MonCompte.js
+++ Trasho/Screens/MonCompte.js
@@ -11,6 +11,37 @@ const dataArray = [
 ];
 
 export default class MonCompte extends Component {
+
+    state = {
+        showPassword: false,
+        showConfirmPassword: false,
+        password: '',
+        confirmPassword: '',
+        icon: "eye-off"
+    }
+
+    _changeIcon() {
+        this.setState(prevState => ({
+            icon: prevState.icon === 'eye' ? 'eye-off' : 'eye',
+            showPassword: !prevState.showPassword
+        }));
+    }
+
+    _changeIconConfirmPassword() {
+        this.setState(prevState => ({
+            icon: prevState.icon === 'eye' ? 'eye-off' : 'eye',
+            showConfirmPassword: !prevState.showConfirmPassword
+        }));
+    }
+
+    handlePassword = (text) => {
+        this.setState({ password: text })
+    }
+
+    handleConfirmPassword = (text) => {
+        this.setState({ confirmPassword: text })
+    }
+
     render() {
         return (
             <Container>
@@ -25,14 +56,16 @@ export default class MonCompte extends Component {
                                     <Input placeholder="Ancien mot de passe" />
                                 </Item>
                                 <Item>
-                                    <Input placeholder="Nouveau mot de passe" />
+                                    <Input placeholder="Nouveau mot de passe" secureTextEntry={!this.state.showPassword} onChangeText={this.handlePassword} />
+                                    <Icon name={this.state.icon} onPress={() => this._changeIcon()} />
                                 </Item>
                                 <Item>
-                                    <Input placeholder="Confirmer nouveau mot de passe" />
+                                    <Input placeholder="Confirmer nouveau mot de passe" secureTextEntry={!this.state.showConfirmPassword} onChangeText={this.handleConfirmPassword} />
+                                    <Icon name={this.state.icon} onPress={() => this._changeIconConfirmPassword()} />
                                 </Item>
                                 <Button rounded block style={[styles.submitButton, styles.buttonWidth]}
                                     onPress={
-                                        () => alert('Validé')
+                                        () => alert('Validé ! Nouveau mdp : ' + this.state.password + ' mdp confirmé : ' + this.state.confirmPassword)
                                     }>
                                     <Text style={styles.submitButtonText}> Valider </Text>
                                 </Button>
@@ -41,24 +74,31 @@ export default class MonCompte extends Component {
                     </Card>
                     <Card>
                         <CardItem bordered style={{ justifyContent: 'center', color: 'black' }}>
-                        <Button transparent style={{color: 'black'}}>
-                        <Icon name={'settings'} style={{color: 'black'}}/>
-                            <Text style={{color: 'black'}}>Récupérer mes données</Text>
-                        </Button>
+                            <Button transparent
+                                onPress={
+                                    () => alert('Récupération des données')
+                                }>
+                                <Icon name={'settings'} style={styles.black} />
+                                <Text style={styles.black}>Récupérer mes données</Text>
+                            </Button>
                         </CardItem>
                     </Card>
                     <Card>
                         <CardItem bordered style={{ justifyContent: 'center', color: 'black' }}>
-                        <Button transparent><Icon name={'person'} style={{color: 'black'}} />
-                            
-                            <Text style={{color: 'black'}}>Supprimer mon compte</Text></Button>
+                            <Button transparent
+                                onPress={
+                                    () => alert('Suppression du compte')
+                                }>
+                                <Icon name={'person'} style={styles.black} />
+                                <Text style={styles.black}>Supprimer mon compte</Text>
+                            </Button>
                         </CardItem>
                     </Card>
-                    <Text style={{marginRight: 'auto', marginLeft: 'auto', marginTop: 30 }}> Niveau </Text>
+                    <Text style={styles.niveau}> Niveau </Text>
                     <Item style={{ borderColor: 'transparent', justifyContent: 'center', marginTop: 15 }}>
-                        <Text style={{marginRight: 5}}>1</Text>
+                        <Text style={{ marginRight: 5 }}>1</Text>
                         <Progress.Bar progress={0.5} width={300} borderColor={'#74992e'} color={'#74992e'} />
-                        <Text style={{marginLeft: 5}}>2</Text>
+                        <Text style={{ marginLeft: 5 }}>2</Text>
                     </Item>
                 </Content>
             </Container>
@@ -84,5 +124,13 @@ const styles = StyleSheet.create({
     buttonWidth: {
         marginLeft: 15,
         marginRight: 15
+    },
+    black: {
+        color: 'black'
+    },
+    niveau: {
+        marginRight: 'auto',
+        marginLeft: 'auto',
+        marginTop: 30
     }
 });
