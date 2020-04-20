6b5309644fb58444bafc9a8b2d8592c72dc56e07
Lucas <lucas.laloux3011@gmail.com>
2020-04-20 19:00:35 +0200

    Ajout des boutons de la page d'admin

--- Trasho/Screens/Admin.js
+++ Trasho/Screens/Admin.js
@@ -1,15 +1,51 @@
 //This is an example code for NavigationDrawer//
 import React, { Component } from 'react';
 //import react in our code.
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Container, Content, Input, Card, CardItem, Text, Body, Item, Button, Icon } from "native-base";
 // import all basic components
 
 export default class Admin extends Component {
     render() {
         return (
-            <View style={styles.MainContainer}>
-                <Text style={{ fontSize: 23 }}> Admin </Text>
-            </View>
+            <Container>
+                <Content padder>
+                    <Card>
+                        <CardItem bordered style={{ justifyContent: 'center', color: 'black' }}>
+                            <Button transparent
+                                onPress={
+                                    () => alert('Liste utilisateurs')
+                                }>
+                                <Icon name={'person'} style={styles.black} />
+                                <Text style={styles.black}>Liste utilisateurs</Text>
+                            </Button>
+                        </CardItem>
+                    </Card>
+                    <Card>
+                        <CardItem bordered style={{ justifyContent: 'center', color: 'black' }}>
+                            <Button transparent
+                                onPress={
+                                    () => alert('Liste poubelles')
+                                }>
+                                <Icon name={'trash'} style={styles.black} />
+                                <Text style={styles.black}>Liste poubelles</Text>
+                            </Button>
+                        </CardItem>
+                    </Card>
+                    <Card>
+                        <CardItem bordered style={{ justifyContent: 'center', color: 'black' }}>
+                            <Button transparent
+                                onPress={
+                                    () => alert('Statistiques')
+                                }>
+                                <Icon name={'stats'} style={styles.black} />
+                                <Text style={styles.black}>Statistiques</Text>
+                            </Button>
+                        </CardItem>
+                    </Card>
+                    
+                </Content>
+            </Container>
         );
     }
 }
@@ -22,4 +58,7 @@ const styles = StyleSheet.create({
         marginTop: 50,
         justifyContent: 'center',
     },
+    black: {
+        color: 'black'
+    }
 });
