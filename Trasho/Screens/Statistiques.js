6e8392b88b78788f38a31fb28fb8da44702bf544
Lucas <lucas.laloux3011@gmail.com>
2020-04-28 18:20:03 +0200

    Quelques ajouts page statistique

--- Trasho/Screens/Statistiques.js
+++ Trasho/Screens/Statistiques.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Container, Content, Input, Card, CardItem, Text, Body, Item, Button, Icon } from "native-base";
 
 export default class Statistiques extends Component {
+
+state = {
+    nbPoubelles: 0,
+    nbSignalements: 0
+}
+
     render() {
         return (
-            <View style={styles.MainContainer}>
-                <Text style={{ fontSize: 23 }}> Statistiques </Text>
-            </View>
+            <Container>
+                <Content padder>
+                <Text style={{ marginRight: 5, marginBottom: 25, fontSize: 30 }}>Depuis 7 jours :</Text>
+                    <Item style={{ justifyContent: 'center', marginTop: 15 }}>
+                        <Text style={{ marginRight: 5, paddingBottom: 15, fontSize: 20 }}>{this.state.nbPoubelles} nouvelles poubelles</Text>
+                    </Item>
+                    <Item style={{ justifyContent: 'center', marginTop: 15 }}>
+                        <Text style={{ marginRight: 5, paddingBottom: 15, fontSize: 20 }}>{this.state.nbSignalements} poubelles signalées</Text>
+                    </Item>
+                </Content>
+            </Container>
         );
     }
 }
