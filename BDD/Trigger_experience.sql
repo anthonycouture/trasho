d1ed35b914e88e6dcdb2ecf7b79ef6ec4ef48daf
Mouloukin <ptitmoulouk62@live.fr>
2020-06-11 14:43:46 +0200

    ADD - Trigger to level-up

---
+++ BDD/Trigger_experience.sql
@@ -0,0 +1,17 @@
+--Il faut ajouter un champs niveau (default 1) dans la base de données !
+
+CREATE OR REPLACE FUNCTION func_maj_niveau() RETURNS TRIGGER AS $maj_niveau$
+	BEGIN
+		IF((NEW.experience >= 100)) THEN
+			NEW.niveau = OLD.niveau + 1;
+			NEW.experience = NEW.experience - 100;
+		END IF;
+		return NEW;
+	END;
+$maj_niveau$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER maj_niveau
+BEFORE UPDATE of experience ON utilisateur
+FOR EACH ROW
+EXECUTE FUNCTION func_maj_niveau();
